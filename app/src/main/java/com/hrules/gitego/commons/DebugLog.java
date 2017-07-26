2ac81c97c038555b5dc4538ba81da97daa8062db
hector <h.deisidro@gmail.com>
2017-07-26 23:49:10 +0200

    Update DebugLog

--- app/src/main/java/com/hrules/gitego/commons/DebugLog.java
+++ app/src/main/java/com/hrules/gitego/commons/DebugLog.java
@@ -16,6 +16,7 @@
 
 package com.hrules.gitego.commons;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 import com.hrules.gitego.BuildConfig;
 
@@ -23,8 +24,10 @@ import com.hrules.gitego.BuildConfig;
  * +info: https://gist.github.com/hrules6872/3e047ef1a7ed2a525104
  */
 
-public class DebugLog {
+public final class DebugLog {
   private static final int LOG_TAG_MAX_LENGTH = 23;
+  private static final String TEXT_NULL_VALUE = "null value";
+  private static final String TEXT_SEPARATOR = " ";
 
   private DebugLog() {
   }
@@ -33,17 +36,17 @@ public class DebugLog {
     return BuildConfig.DEBUG;
   }
 
-  private static String formatMessage(String message) {
+  private static @NonNull String formatMessage(@NonNull String message) {
     StackTraceElement[] stackTrace = (new Throwable().getStackTrace());
     if (stackTrace.length >= 2) {
-      return String.format("[%s:%s] -> %s(): %s", stackTrace[2].getFileName(), stackTrace[2].getLineNumber(),
-          stackTrace[2].getMethodName(), message);
+      return String.format("[%s:%s] -> %s(): %s", stackTrace[2].getFileName(), stackTrace[2].getLineNumber(), stackTrace[2].getMethodName(),
+          message);
     } else {
       return message;
     }
   }
 
-  private static String getApplicationId() {
+  private static @NonNull String getApplicationId() {
     String applicationId = BuildConfig.APPLICATION_ID;
     if (applicationId.length() > LOG_TAG_MAX_LENGTH) {
       applicationId = applicationId.substring(0, LOG_TAG_MAX_LENGTH);
@@ -51,61 +54,72 @@ public class DebugLog {
     return applicationId;
   }
 
-  public static void d(Object message) {
+  @SuppressWarnings("ConstantConditions") private static @NonNull String concatenate(@NonNull Object[] objects) {
+    StringBuilder stringBuilder = new StringBuilder();
+    if (objects == null) {
+      objects = new String[] { TEXT_NULL_VALUE };
+    }
+    for (Object o : objects) {
+      stringBuilder.append(o).append(TEXT_SEPARATOR);
+    }
+    return stringBuilder.toString();
+  }
+
+  public static void d(@NonNull Object... messages) {
     if (isDebuggable()) {
-      Log.d(getApplicationId(), formatMessage(String.valueOf(message)));
+      Log.d(getApplicationId(), formatMessage(concatenate(messages)));
     }
   }
 
-  public static void d(Object message, Throwable tr) {
+  public static void d(@NonNull Object message, @NonNull Throwable tr) {
     if (isDebuggable()) {
       Log.d(getApplicationId(), formatMessage(String.valueOf(message)), tr);
     }
   }
 
-  public static void e(Object message) {
+  public static void e(@NonNull Object... messages) {
     if (isDebuggable()) {
-      Log.e(getApplicationId(), formatMessage(String.valueOf(message)));
+      Log.e(getApplicationId(), formatMessage(concatenate(messages)));
     }
   }
 
-  public static void e(Object message, Throwable tr) {
+  public static void e(@NonNull Object message, @NonNull Throwable tr) {
     if (isDebuggable()) {
       Log.e(getApplicationId(), formatMessage(String.valueOf(message)), tr);
     }
   }
 
-  public static void i(Object message) {
+  public static void i(@NonNull Object... messages) {
     if (isDebuggable()) {
-      Log.i(getApplicationId(), formatMessage(String.valueOf(message)));
+      Log.i(getApplicationId(), formatMessage(concatenate(messages)));
     }
   }
 
-  public static void i(Object message, Throwable tr) {
+  public static void i(@NonNull Object message, @NonNull Throwable tr) {
     if (isDebuggable()) {
       Log.i(getApplicationId(), formatMessage(String.valueOf(message)), tr);
     }
   }
 
-  public static void v(Object message) {
+  public static void v(@NonNull Object... messages) {
     if (isDebuggable()) {
-      Log.v(getApplicationId(), formatMessage(String.valueOf(message)));
+      Log.v(getApplicationId(), formatMessage(concatenate(messages)));
     }
   }
 
-  public static void v(Object message, Throwable tr) {
+  public static void v(@NonNull Object message, @NonNull Throwable tr) {
     if (isDebuggable()) {
       Log.v(getApplicationId(), formatMessage(String.valueOf(message)), tr);
     }
   }
 
-  public static void w(Object message) {
+  public static void w(@NonNull Object... messages) {
     if (isDebuggable()) {
-      Log.w(getApplicationId(), formatMessage(String.valueOf(message)));
+      Log.w(getApplicationId(), formatMessage(concatenate(messages)));
     }
   }
 
-  public static void w(Object message, Throwable tr) {
+  public static void w(@NonNull Object message, @NonNull Throwable tr) {
     if (isDebuggable()) {
       Log.w(getApplicationId(), formatMessage(String.valueOf(message)), tr);
     }
